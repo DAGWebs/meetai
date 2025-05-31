d820b95470579bc607f4134db6f2d519abf02601
DAGWEBS <kaseyber12@gmail.com>
2025-05-31 18:36:31 -0500

    09: Agents Setup

---
+++ src/app/(dashboard)/agents/page.tsx
@@ -0,0 +1,36 @@
+import { LoadingState } from "@/components/loading-state";
+import { AgentsView } from "@/modules/agents/ui/agents-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import { ErrorState } from "@/components/error-state";
+
+const page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense
+        fallback={
+          <LoadingState
+            title="Loading Agents"
+            description="This may take a few seconds."
+          />
+        }
+      >
+        <ErrorBoundary
+          fallback={
+            <ErrorState
+              title="Something went wrong"
+              description="Please try refreshing the page or coming back later."
+            />
+          }
+        ></ErrorBoundary>
+        <AgentsView />
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
+
+export default page;
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,9 +1,5 @@
 "use client";
 
 export function HomeView({}) {
-  return (
-    <div className="flex flex-col p-4 gap-y-4">
-      <h1 className="text-3xl text-center font-bold">Hello World</h1>
-    </div>
-  );
+  return <div>Home View</div>;
 }
